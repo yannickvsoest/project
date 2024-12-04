40762589b89c16fad85de35a3e17d9af77c92e4d
Gavgastyolo <163116652+gmeijerwebdesign@users.noreply.github.com>
2024-12-04 11:16:55 +0100

    second change

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -39,7 +39,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                     href={route("dashboard")}
                     className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                   >
-                    Dashboarddddddd
+                    Dashboard
                   </Link>
                 ) : (
                   <>
