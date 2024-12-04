06957678d707aca2268feb031a3cdb91fea89e2c
Gavgastyolo <163116652+gmeijerwebdesign@users.noreply.github.com>
2024-12-04 15:15:25 +0100

    revert

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['Component']}.jsx"])
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
         
     </head>
